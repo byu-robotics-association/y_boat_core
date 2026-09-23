c7266ce9b2d41adadb0ff1a426e6ac24950a4de6
nalyd008 <dylancolehale@gmail.com>
2026-09-23 11:48:23 -0600

    Source correct files in docker image

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 set -e
 
-if [ -f "/opt/ros/${ROS_DISTRO}/setup.bash" ]; then
-  source "/opt/ros/${ROS_DISTRO}/setup.bash"
-elif [ -f "/opt/ros/${ROS_DISTRO}/install/setup.bash" ]; then
-  source "/opt/ros/${ROS_DISTRO}/install/setup.bash"
-fi
-
-if [ -f "/workspace/install/setup.bash" ]; then
-  source "/workspace/install/setup.bash"
-fi
-
 exec "$@"
