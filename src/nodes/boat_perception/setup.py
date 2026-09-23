2fc1b9859cc32c946ecba630054c59ab91e6e8f8
Josh McConkie <65884446+JoshMcConkie@users.noreply.github.com>
2026-09-22 23:28:57 -0600

    Add runnable boat perception node scaffold. (#4)

---
+++ src/nodes/boat_perception/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'boat_perception'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Josh McConkie',
+    maintainer_email='joshuamcconkie@gmail.com',
+    description='Package for handling perception on the BYU RoboBoat.',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'lidar_processor = boat_perception.lidar_processor:main',
+        ],
+    },
+)
