8915a0b1fca6f9d04506e62308889deccdbfaac6
Nalyd <dylancolehale@gmail.com>
2026-09-22 22:43:08 -0600

    Merge updated entrypoint.sh handling.
    * This is my commit
    
    * Fix entrypoint.sh
    
    * Delete src/main.cpp
    
    Empty file. Deleted during PR review.
    
    ---------
    
    Co-authored-by: Josh McConkie <65884446+JoshMcConkie@users.noreply.github.com>

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-source /opt/ros/${ROS_DISTRO}/setup.bash
-
 if [ -f "/opt/ros/${ROS_DISTRO}/setup.bash" ]; then
   source "/opt/ros/${ROS_DISTRO}/setup.bash"
 elif [ -f "/opt/ros/${ROS_DISTRO}/install/setup.bash" ]; then
